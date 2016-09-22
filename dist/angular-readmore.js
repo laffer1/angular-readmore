e6ccce9553f8b45f765dbeafd7c6b01be2d5d4ab
Lucas Holt <luke@foolishgames.com>
2016-09-22 14:52:40 -0400

    1.0.1 release includes bug fixes to ellipsis and code cleanup.

--- dist/angular-readmore.js
+++ dist/angular-readmore.js
@@ -1,6 +1,6 @@
 /**
  * AngularJS 1.x library to cut text and provide a read more/ellipsis
- * @version v1.0.0 - 2016-09-22
+ * @version v1.0.1 - 2016-09-22
  * @link http://github.com/laffer1/angular-readmore
  * @author Lucas Holt <luke@foolishgames.com>,Joyce Cam
  * @license MIT License, http://www.opensource.org/licenses/MIT
@@ -11,85 +11,92 @@
  * Simple and easy-to-implement angular read more directive.
  *
  */
+(function () {
+    'use strict';
 
-var readMore = angular.module('readMore', []);
+    angular.module('readMore', [])
+            .directive('readMore', readMore);
 
-readMore.directive('readMore', function () {
-    return {
-        restrict: 'A',
-        transclude: true,
-        replace: true,
-        template: '<p></p>',
-        scope: {
-            moreText: '@',
-            lessText: '@',
-            words: '@',
-            ellipsis: '@',
-            char: '@',
-            limit: '@',
-            content: '@'
-        },
-        link: function (scope, elem, attr, ctrl, transclude) {
-            var moreText = angular.isUndefined(scope.moreText) ? ' <a class="read-more">Read More...</a>' : ' <a class="read-more">' + scope.moreText + '</a>',
-                    lessText = angular.isUndefined(scope.lessText) ? ' <a class="read-less">Less ^</a>' : ' <a class="read-less">' + scope.lessText + '</a>',
-                    ellipsis = angular.isUndefined(scope.ellipsis) ? '' : scope.ellipsis,
-                    limit = angular.isUndefined(scope.limit) ? 150 : scope.limit;
+    function readMore() {
+        return {
+            restrict: 'A',
+            transclude: true,
+            replace: true,
+            template: '<p></p>',
+            scope: {
+                moreText: '@',
+                lessText: '@',
+                words: '@',
+                ellipsis: '@',
+                char: '@',
+                limit: '@',
+                content: '@'
+            },
+            link: function (scope, elem, attr, ctrl, transclude) {
+                var moreText = angular.isUndefined(scope.moreText) ? ' <a class="read-more">Read More...</a>' : ' <a class="read-more">' + scope.moreText + '</a>';
+                var lessText = angular.isUndefined(scope.lessText) ? ' <a class="read-less">Less ^</a>' : ' <a class="read-less">' + scope.lessText + '</a>';
+                var ellipsis = angular.isUndefined(scope.ellipsis) ? '' : scope.ellipsis;
+                var limit = angular.isUndefined(scope.limit) ? 150 : scope.limit;
 
-            function readmore(text) {
-                var orig = text;
-                var regex = /\s+/gi;
-                var charCount = text.length;
-                var wordCount = text.trim().replace(regex, ' ').split(' ').length;
-                var countBy = 'char';
-                var count = charCount;
-                var foundWords = [];
-                var markup = text;
-                var more = '';
+                function readmore(text) {
+                    var orig = text;
+                    var regex = /\s+/gi;
+                    var charCount = text.length;
+                    var wordCount = text.trim().replace(regex, ' ').split(' ').length;
+                    var countBy = 'char';
+                    var count = charCount;
+                    var foundWords = [];
+                    var markup = text;
+                    var more = '';
+                    var ellipsisMarkup = '<span class="ellipsis">'+ellipsis+ '</span>';
 
-                if (!angular.isUndefined(attr.words)) {
-                    countBy = 'words';
-                    count = wordCount;
-                }
-
-                if (countBy === 'words') { // Count words
-
-                    foundWords = text.split(/\s+/);
-
-                    if (foundWords.length > limit) {
-                        text = foundWords.slice(0, limit).join(' ') + ellipsis;
-                        more = foundWords.slice(limit, count).join(' ');
-                        markup = text + moreText + '<span class="more-text">' + more + lessText + '</span>';
+                    if (!angular.isUndefined(attr.words)) {
+                        countBy = 'words';
+                        count = wordCount;
                     }
 
-                } else { // Count characters
+                    if (countBy === 'words') { // Count words
 
-                    if (count > limit) {
-                        text = orig.slice(0, limit) + ellipsis;
-                        more = orig.slice(limit, count);
-                        markup = text + moreText + '<span class="more-text">' + more + lessText + '</span>';
+                        foundWords = text.split(/\s+/);
+
+                        if (foundWords.length > limit) {
+                            text = foundWords.slice(0, limit).join(' ');
+                            more = foundWords.slice(limit, count).join(' ');
+                            markup = text + ellipsisMarkup + moreText + '<span class="more-text">' + more + lessText + '</span>';
+                        }
+
+                    } else { // Count characters
+
+                        if (count > limit) {
+                            text = orig.slice(0, limit);
+                            more = orig.slice(limit, count);
+                            markup = text + ellipsisMarkup + moreText + '<span class="more-text">' + more + lessText + '</span>';
+                        }
+
                     }
+
+                    elem.append(markup);
+                    elem.find('.read-more').on('click', function () {
+                        $(this).hide();
+                        elem.find('.more-text').addClass('show').slideDown();
+                        elem.find('.ellipsis').hide();
+                    });
+                    elem.find('.read-less').on('click', function () {
+                        elem.find('.read-more').show();
+                        elem.find('.more-text').hide().removeClass('show');
+                        elem.find('.ellipsis').show();
+                    });
 
                 }
 
-                elem.append(markup);
-                elem.find('.read-more').on('click', function () {
-                    $(this).hide();
-                    elem.find('.more-text').addClass('show').slideDown();
-                });
-                elem.find('.read-less').on('click', function () {
-                    elem.find('.read-more').show();
-                    elem.find('.more-text').hide().removeClass('show');
+                attr.$observe('content', function (str) {
+                    readmore(str);
                 });
 
+                transclude(scope.$parent, function (clone, scope) {
+                    readmore(clone.text().trim());
+                });
             }
-
-            attr.$observe('content', function (str) {
-                readmore(str);
-            });
-
-            transclude(scope.$parent, function (clone, scope) {
-                readmore(clone.text().trim());
-            });
-        }
-    };
-});
+        };
+    }
+})();
